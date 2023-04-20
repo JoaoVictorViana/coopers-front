5237c00ac6a8271291be3b9f1b8875fc284b750f
João Victor Viana <joao.duarte@smplaces.com.br>
2023-04-20 13:43:47 -0300

    feat(describe): implementação do layout da descrição do to-do list

---
+++ src/components/feature/Describe/index.tsx
@@ -0,0 +1,11 @@
+export const Describe = () => {
+  return (
+    <section className="Describe">
+      <h2 className="Describe__title">To-do List</h2>
+      <span className="Describe__description">
+        Drag and drop to set your main priorities, check when done and create
+        what´s new.
+      </span>
+    </section>
+  )
+}
